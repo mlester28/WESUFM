cadd624c6001854d8dd116290e869ed61c3af09c
Miriam Lester <miriam.lester@loopup.com>
2015-10-01 22:49:51 -0400

    First version of app that plays and pauses.

---
+++ WESUFM/RadioPlayer.swift
@@ -0,0 +1,39 @@
+//
+//  RadioPlayer.swift
+//  WESUFM
+//
+//  Created by Miriam Lester on 9/29/15.
+//  Copyright (c) 2015 WESUFM. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class RadioPlayer {
+    static let sharedInstance = RadioPlayer()
+    private var player = AVPlayer(URL: NSURL(string: "http://radio.wesleyan.edu:8000/stream.m3u"))
+    private var isPlaying = false
+    
+    func play() {
+        player.play()
+        isPlaying = true
+    }
+    
+    func pause() {
+        player.pause()
+        isPlaying = false
+    }
+    
+    func toggle() {
+        if isPlaying == true {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func currentlyPlaying() -> Bool {
+        return isPlaying
+    }
+    
+}
--- WESUFM/ViewController.swift
+++ WESUFM/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var titleImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //playButton.setTitle("Play", forState: UIControlState.Normal)
+        titleImage.image = UIImage(named: "radioCroped")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        toggle()
+    }
+
+    func toggle() {
+        if RadioPlayer.sharedInstance.currentlyPlaying() {
+            pauseRadio()
+        } else {
+            playRadio()
+        }
+    }
+    
+    func playRadio() {
+        RadioPlayer.sharedInstance.play()
+        //playButton.setTitle("Pause", forState: UIControlState.Normal)
+        playButton.setBackgroundImage(UIImage(named: "pauseRound"), forState: UIControlState.Normal)
+    }
+    
+    func pauseRadio() {
+        RadioPlayer.sharedInstance.pause()
+        //playButton.setTitle("Play", forState: UIControlState.Normal)
+        playButton.setBackgroundImage(UIImage(named: "playRound"), forState: UIControlState.Normal)
+        
+    }
 
 }
 
